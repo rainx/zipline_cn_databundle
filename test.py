78ea3248d1fc553517e6301c6b425dcf87c3ff9b
RainX <1020682330@qq.com>
2016-08-19 11:26:06 +0800

    add ok

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
-from zipline_cn_databundle import register_cn_bundle_from_yahoo, get_all_stocks
+from zipline_cn_databundle import register_cn_bundle_from_yahoo, get_all_stocks, get_all_yahoo_stock_names, check_code
 from pprint import  pprint
 
 
 
 if __name__ == '__main__':
-    all = get_all_stocks()
-    print(all)
+    symbols = get_all_yahoo_stock_names()
+    filtered_symbols = list(filter(check_code, symbols))
+    print('output to symbols.txt')
+    with open('symbols.txt', 'w') as f:
+        f.write("\n".join(filtered_symbols))
+    print('done!')
 
 
 
--- zipline_cn_databundle/__init__.py
+++ zipline_cn_databundle/__init__.py
@@ -1,7 +1,9 @@
 from .yahoo import (
     register_cn_bundle_from_yahoo,
+    get_all_yahoo_stock_names,
+    check_code
 )
 
 from .all_stocks import (
-        get_all_stocks
+        get_all_stocks,
 )
