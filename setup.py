667319410d6210c75f75e5519d64f128bbac55ce
RainX <1020682330@qq.com>
2016-10-28 14:12:20 +0800

    add gen all index command

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     entry_points={
         'console_scripts': [
             'zipline-cn-databundle-update=zipline_cn_databundle:zipline_cn_databundle_update',
+            'gen-all-index-benchmark-data=zipline_cn_databundle.index_list.__init__:gen_data',
         ]
     }
 )
